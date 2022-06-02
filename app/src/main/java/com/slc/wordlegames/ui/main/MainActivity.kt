8912d8e7cdfbb1f015e1edd1ed99801570b4e97c
Sergio <sergioloc2@gmail.com>
2022-06-02 14:30:34 +0200

    New basic webview

--- app/src/main/java/com/slc/wordlegames/ui/main/MainActivity.kt
+++ app/src/main/java/com/slc/wordlegames/ui/main/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.slc.wordlegames.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.slc.wordlegames.R
+import com.slc.wordlegames.ui.webview.WebViewActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this, WebViewActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/slc/wordlegames/ui/webview/WebViewActivity.kt
@@ -0,0 +1,18 @@
+package com.slc.wordlegames.ui.webview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.slc.wordlegames.databinding.ActivityWebViewBinding
+
+class WebViewActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWebViewBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWebViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.webView.loadUrl("https://www.google.es/")
+    }
+}
