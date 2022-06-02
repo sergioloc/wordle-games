46eb8a4210b5161c6c50e0f345a619c2f4d1f747
Sergio <sergioloc2@gmail.com>
2022-06-02 16:32:53 +0200

    New menu adapter

--- app/src/main/java/com/slc/wordlegames/ui/menu/MenuActivity.kt
+++ app/src/main/java/com/slc/wordlegames/ui/menu/MenuActivity.kt
@@ -1,33 +1,43 @@
 package com.slc.wordlegames.ui.menu
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
-import com.slc.wordlegames.R
-import com.slc.wordlegames.ui.webview.WebViewActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.slc.wordlegames.databinding.ActivityMenuBinding
+import com.slc.wordlegames.domain.model.Game
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MenuActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity(), MenuAdapter.OnGameClickListener {
 
+    private lateinit var binding: ActivityMenuBinding
     private val viewModel: MenuViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         //startActivity(Intent(this, WebViewActivity::class.java))
 
+        initView()
         initObservers()
         viewModel.getGames()
+    }
+
+    private fun initView() {
+        binding.rvGames.layoutManager = LinearLayoutManager(this)
     }
 
     private fun initObservers() {
         viewModel.games.observe(this) {
             it.onSuccess { list ->
-                Log.i("TAGTAG", list.size.toString())
+                binding.rvGames.adapter = MenuAdapter(list, this)
             }
         }
+    }
+
+    override fun onClickGame(game: Game) {
+
     }
 }
---
+++ app/src/main/java/com/slc/wordlegames/ui/menu/MenuAdapter.kt
@@ -0,0 +1,40 @@
+package com.slc.wordlegames.ui.menu
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.slc.wordlegames.R
+import com.slc.wordlegames.domain.model.Game
+
+class MenuAdapter(private var games: List<Game>, private val listener: OnGameClickListener): RecyclerView.Adapter<MenuAdapter.UserViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        return UserViewHolder(LayoutInflater.from(parent.context), parent)
+    }
+
+    override fun getItemCount(): Int = games.size
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val g = games[position]
+
+        holder.tvName.text = g.name
+
+    }
+
+    class UserViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
+        RecyclerView.ViewHolder(inflater.inflate(R.layout.cell_game, parent, false)) {
+
+        var root: ConstraintLayout = itemView.findViewById(R.id.card)
+        var ivIcon: ImageView = itemView.findViewById(R.id.ivIcon)
+        var tvName: TextView = itemView.findViewById(R.id.tvName)
+
+    }
+
+    interface OnGameClickListener {
+        fun onClickGame(game: Game)
+    }
+
+}
