59c82085b8e105a1d50b3cc2baae529d70cf3b1e
Sergio <sergioloc2@gmail.com>
2022-06-08 12:51:34 +0200

    New hidden activity

---
+++ app/src/main/java/com/slc/wordlegames/ui/HiddenActivity.kt
@@ -0,0 +1,12 @@
+package com.slc.wordlegames.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.slc.wordlegames.R
+
+class HiddenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hidden)
+    }
+}
--- app/src/main/java/com/slc/wordlegames/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/slc/wordlegames/ui/settings/SettingsActivity.kt
@@ -1,12 +1,26 @@
 package com.slc.wordlegames.ui.settings
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.slc.wordlegames.R
+import com.slc.wordlegames.databinding.ActivitySettingsBinding
+import com.slc.wordlegames.ui.HiddenActivity
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initButtons()
+    }
+
+    private fun initButtons() {
+        binding.tvHidden.setOnClickListener {
+            startActivity(Intent(this, HiddenActivity::class.java))
+        }
     }
 }
